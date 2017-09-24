c908e28cd1053cb923229d536d3d7ebde97fcecb
Gump <yyangcheng@hotmail.com>
2017-09-24 11:34:11 +0800

    加入一个简单的平均以进行数据过滤

--- wechat_app/pages/index/index.js
+++ wechat_app/pages/index/index.js
@@ -9,7 +9,21 @@ var rooms = {
     2: '大厅',
     3: '办公室一',
     4: '办公室二'
-}
+};
+
+var dataArray = {
+  1: [],
+  2: [],
+  3: [],
+  4: []
+};
+
+var countArray = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0
+};
 
 Page({
   data: {
@@ -18,8 +32,10 @@ Page({
     searchingState: '扫描中...',
     allBeacons:[]
   },
+
   onLoad: function () {
-      this.scanBeacon()
+      this.scanBeacon();
+      setInterval(this.displayReload, 2000);
   },
 
   scanBeacon() {
@@ -30,31 +46,69 @@ Page({
           success(res) {
               console.log('startBeaconDiscovery success')
               wx.onBeaconUpdate((res) => {
-                  let beacons = res.beacons
-                  let text = '未找到设备'
-                  that.loadingState()
-                  if (beacons.length > 0) {
-                      let nearest = 0
-                      let accur = 9999
-                      let room = '未知'
-                      that.setData({allBeacons: beacons})
-                      beacons.forEach((b) => {
-                          if (b.accuracy < 0) {
-                            return;
-                          }
-                          if (b.accuracy < accur && rooms[b.minor] != undefined) {
-                              nearest = b.minor
-                              accur = b.accuracy
-                              room = rooms[nearest]
-                          }
-                      })
-                      text = '当前位置：' + room
-                  }
-                  console.log('beacon count=' + beacons.length, beacons)
-                  that.setData({ beaconState: text })
+                let beacons = res.beacons
+                // let text = '未找到设备'
+                that.loadingState()
+                if (beacons.length > 0) {
+                    // let nearest = 0
+                    // let accur = 9999
+                    // let room = '未知'
+                    // that.setData({allBeacons: beacons})
+                    beacons.forEach((b) => {
+                        if (b.accuracy < 0) {
+                          return;
+                        }
+                        that.pushData(b);
+                        /*
+                        if (b.accuracy < accur && rooms[b.minor] != undefined) {
+                            nearest = b.minor
+                            accur = b.accuracy
+                            room = rooms[nearest]
+                        }*/
+                    })
+                    // text = '当前位置：' + room
+                }
+                // console.log('beacon count=' + beacons.length, beacons)
+                // that.setData({ beaconState: text })
               })
           }
       })
+  },
+
+  pushData(beacon) {
+    var i = beacon.minor;
+    if (rooms[i] != undefined) {
+      countArray[i] = dataArray[i].push(beacon.accuracy);
+    }
+  },
+
+  displayReload() {
+    let text = '未找到设备';
+    console.log('count:', countArray);
+    let nearest = 0
+    let accur = 9999
+    let room = '未知'
+    let beacons = [];
+    for (var i=1; i<5; i++) {
+      var d = dataArray[i];
+      if (d.length > 0) {
+        var sum = 0;
+        d.forEach((v) => {
+          sum += v;
+        });
+        sum /= d.length;
+        console.log("" + i, " sum=" + sum);
+        if (sum < accur) {
+          accur = sum;
+          nearest = i;
+          room = rooms[nearest];
+        }
+        beacons.push({ minor: i, accuracy: sum});
+      }
+    }
+    text = '当前位置：' + room;
+    this.setData({ beaconState: text, allBeacons: beacons });
+    dataArray[1].length = dataArray[2].length = dataArray[3].length = dataArray[4].length = 0;
   },
 
   currentState: 1,
